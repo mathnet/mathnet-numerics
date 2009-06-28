1bf8cef2ed9f3b5a8cde2ffdff7d54a68df9a674
Marcus Cuda <marcus@cuda.net>
2009-06-28 17:24:53 +0300

    Added missing files
    Signed-off-by: Christoph Ruegg <git@cdrnet.ch>

---
+++ src/Managed.UnitTests/ComplexTest.cs
@@ -0,0 +1,16 @@
+﻿namespace MathNet.Numerics.UnitTests
+{
+    using MbUnit.Framework;
+
+    [TestFixture]
+    public class ComplexTest
+    {
+        [Test, MultipleAsserts]
+        public void CanCreateAComplexNumberUsingTheConstructor()
+        {
+            var complex = new Complex(1.1, -2.2);
+            AssertEx.That(() => complex.Real == 1.1, "Real Part");
+            AssertEx.That(() => complex.Imaginary == -2.2, "Imaginary Part");
+        }
+    }
+}
