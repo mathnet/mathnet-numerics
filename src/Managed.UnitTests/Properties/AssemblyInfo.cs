4156454fac882ff1aadb09261a658c6c4120c41b
Marcus Cuda <marcus@cuda.net>
2009-06-28 14:53:59 +0800

    renamed Managed.Test to Managed.UnitTests since we'll multiple types of tests and added Native.UnitTests project
    Signed-off-by: Marcus Cuda <marcus@cuda.net>

--- src/Managed.UnitTests/Properties/AssemblyInfo.cs
+++ src/Managed.UnitTests/Properties/AssemblyInfo.cs
@@ -1,10 +1,12 @@
 ﻿using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
+// General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Math.NET Numerics (Test)")]
+[assembly: AssemblyTitle("Math.NET Numerics (Managed) Unit Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Math.NET Project")]
@@ -13,18 +15,18 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("2e405a5c-3b48-40e0-adf2-6f623b18373f")]
+[assembly: Guid("475dd2ad-3fa9-42df-9790-4a444ed886ce")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version
+//      Minor Version 
 //      Build Number
 //      Revision
 //
@@ -33,3 +35,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en")]
