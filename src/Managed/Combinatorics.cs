e3c987760e655428cb9a9328641edf9f783e907f
Christoph Ruegg <git@cdrnet.ch>
2009-06-30 06:02:24 +0800

    Api Suggestion for combinatorics/counting
    Signed-off-by: Marcus Cuda <marcus@cuda.net>

---
+++ src/Managed/Combinatorics.cs
@@ -0,0 +1,70 @@
+﻿// <copyright file="Combinatorics.cs" company="Math.NET">
+// Math.NET Numerics, part of the Math.NET Project
+// http://mathnet.opensourcedotnet.info
+//
+// Copyright (c) 2009 Math.NET
+//
+// Permission is hereby granted, free of charge, to any person
+// obtaining a copy of this software and associated documentation
+// files (the "Software"), to deal in the Software without
+// restriction, including without limitation the rights to use,
+// copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following
+// conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+// </copyright>
+
+namespace MathNet.Numerics
+{
+    using System;
+
+    /// <summary>
+    /// Enumerative Combinatorics and Counting.
+    /// </summary>
+    public static class Combinatorics
+    {
+        /// <summary>
+        /// Computes the number of variations without repetition. The order matters and each object can be chosen only once.
+        /// </summary>
+        /// <param name="n">Number of elements in the set.</param>
+        /// <param name="k">Number of elements to choose from the set. Each element is chosen at most once.</param>
+        /// <returns>Maximum number of distinct variations.</returns>
+        public static double Variations(int n, int k)
+        {
+            if(k < 0 || n < 0 || k > n)
+            {
+                return 0;
+            }
+
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Computes the number of variations with repetition. The order matters and each object can be chosen more than once.
+        /// </summary>
+        /// <param name="n">Number of elements in the set.</param>
+        /// <param name="k">Number of elements to choose from the set. Each element is chosen 0, 1 or multiple times.</param>
+        /// <returns>Maximum number of distinct variations with repetition.</returns>
+        public static double VariationsWithRepetition(int n, int k)
+        {
+            if(k < 0 || n < 0)
+            {
+                return 0;
+            }
+
+            return Math.Pow(n, k);
+        }
+    }
+}
