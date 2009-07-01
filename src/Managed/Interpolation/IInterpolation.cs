4f6d8fd52449823e6abf68064ed4869ceb4851a9
Christoph Ruegg <git@cdrnet.ch>
2009-07-01 22:26:43 +0800

    interpolation: factory and interface suggestion
    Signed-off-by: Christoph Ruegg <git@cdrnet.ch>

---
+++ src/Managed/Interpolation/IInterpolation.cs
@@ -0,0 +1,87 @@
+﻿// <copyright file="IInterpolation.cs" company="Math.NET">
+// Math.NET Numerics, part of the Math.NET Project
+// http://mathnet.opensourcedotnet.info
+//
+// Copyright (c) 2009 Math.NET
+//
+// Permission is hereby granted, free of charge, to any person
+// obtaining a copy of this software and associated documentation
+// files (the "Software"), to deal in the Software without
+// restriction, including without limitation the rights to use,
+// copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following
+// conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+// </copyright>
+
+namespace MathNet.Numerics.Interpolation
+{
+    /// <summary>
+    /// Interpolation within the range of a discrete set of known data points.
+    /// </summary>
+    public interface IInterpolation
+    {
+        /// <summary>
+        /// Gets a value indicating whether the algorithm supports differentiation (interpolated derivative).
+        /// </summary>
+        /// <seealso cref="Differentiate(double)"/>
+        /// <seealso cref="Differentiate(double, out double, out double)"/>
+        bool SupportsDifferentiation { get; }
+
+        /// <summary>
+        /// Gets a value indicating whether the algorithm supports integration (interpolated quadrature).
+        /// </summary>
+        /// <seealso cref="Integrate"/>
+        bool SupportsIntegration { get; }
+
+        /// <summary>
+        /// Interpolate at point t.
+        /// </summary>
+        /// <param name="t">Point t to interpolate at.</param>
+        /// <returns>Interpolated value x(t).</returns>
+        double Interpolate(double t);
+
+        /// <summary>
+        /// Differentiate at point t.
+        /// </summary>
+        /// <param name="t">Point t to interpolate at.</param>
+        /// <returns>Interpolated first derivative at point t.</returns>
+        /// <seealso cref="SupportsDifferentiation"/>
+        /// <seealso cref="Differentiate(double, out double, out double)"/>
+        double Differentiate(double t);
+
+        /// <summary>
+        /// Differentiate at point t.
+        /// </summary>
+        /// <param name="t">Point t to interpolate at.</param>
+        /// <param name="interpolatedValue">Interpolated value x(t)</param>
+        /// <param name="secondDerivative">Interpolated second derivative at point t.</param>
+        /// <returns>Interpolated first derivative at point t.</returns>
+        /// <seealso cref="SupportsDifferentiation"/>
+        /// <seealso cref="Differentiate(double)"/>
+        double Differentiate(
+            double t,
+            out double interpolatedValue,
+            out double secondDerivative);
+
+        /// <summary>
+        /// Integrate up to point t.
+        /// </summary>
+        /// <param name="t">Right bound of the integration interval [a,t].</param>
+        /// <returns>Interpolated definite integral over the interval [a,t].</returns>
+        /// <seealso cref="SupportsIntegration"/>
+        double Integrate(double t);
+    }
+}
---
+++ src/Managed/Interpolation/Interpolation.cs
@@ -0,0 +1,90 @@
+﻿// <copyright file="Interpolation.cs" company="Math.NET">
+// Math.NET Numerics, part of the Math.NET Project
+// http://mathnet.opensourcedotnet.info
+//
+// Copyright (c) 2009 Math.NET
+//
+// Permission is hereby granted, free of charge, to any person
+// obtaining a copy of this software and associated documentation
+// files (the "Software"), to deal in the Software without
+// restriction, including without limitation the rights to use,
+// copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following
+// conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+// </copyright>
+
+namespace MathNet.Numerics.Interpolation
+{
+    using System;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Interpolation Factory.
+    /// </summary>
+    public static class Interpolation
+    {
+        /// <summary>
+        /// Create a rational pole-free interpolation based on arbitrary points. This is the default interpolation scheme.
+        /// </summary>
+        /// <param name="points">The sample points t. Supports both lists and arrays.</param>
+        /// <param name="values">The sample point values x(t). Supports both lists and arrays.</param>
+        /// <returns>
+        /// An interpolation scheme optimized for the given sample points and values,
+        /// which can then be used to compute interpolations and extrapolations
+        /// on arbitrary points.
+        /// </returns>
+        public static IInterpolation Create(
+            IList<double> points,
+            IList<double> values)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Create a linear spline interpolation based on arbitrary points.
+        /// </summary>
+        /// <param name="points">The sample points t. Supports both lists and arrays.</param>
+        /// <param name="values">The sample point values x(t). Supports both lists and arrays.</param>
+        /// <returns>
+        /// An interpolation scheme optimized for the given sample points and values,
+        /// which can then be used to compute interpolations and extrapolations
+        /// on arbitrary points.
+        /// </returns>
+        public static IInterpolation CreateLinearSpline(
+            IList<double> points,
+            IList<double> values)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Create a rational pole-free interpolation based on arbitrary points.
+        /// </summary>
+        /// <param name="points">The sample points t. Supports both lists and arrays.</param>
+        /// <param name="values">The sample point values x(t). Supports both lists and arrays.</param>
+        /// <returns>
+        /// An interpolation scheme optimized for the given sample points and values,
+        /// which can then be used to compute interpolations and extrapolations
+        /// on arbitrary points.
+        /// </returns>
+        public static IInterpolation CreateRationalPoleFree(
+            IList<double> points,
+            IList<double> values)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
