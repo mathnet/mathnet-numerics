3ae322ef98fc4c7d9b466e0d9557cfc47ffbe3ec
Christoph Ruegg <git@cdrnet.ch>
2009-07-27 04:31:57 +0800

    quadrature: ported from iridium
    Signed-off-by: Christoph Ruegg <git@cdrnet.ch>

---
+++ src/Managed.UnitTests/IntegrationTests/IntegrationTest.cs
@@ -0,0 +1,155 @@
+﻿// <copyright file="IntegrationTest.cs" company="Math.NET">
+// Math.NET Numerics, part of the Math.NET Project
+// http://mathnet.opensourcedotnet.info
+//
+// Copyright (c) 2009 Math.NET
+//
+// Permission is hereby granted, free of charge, to any person
+// obtaining a copy of this software and associated documentation
+// files (the "Software"), to deal in the Software without
+// restriction, including without limitation the rights to use,
+// copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following
+// conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+// </copyright>
+
+
+namespace MathNet.Numerics.UnitTests.IntegrationTests
+{
+    using System;
+    using Integration;
+    using Integration.Algorithms;
+    using MbUnit.Framework;
+
+    [TestFixture]
+    public class IntegrationTest
+    {
+        /// <summary>
+        /// Test Function: f(x) = exp(-x/5) (2 + sin(2 * x))
+        /// </summary>
+        private static double TargetFunctionA(double x)
+        {
+            return Math.Exp(-x / 5) * (2 + Math.Sin(2 * x));
+        }
+
+        private const double StartA = 0;
+        private const double StopA = 10;
+        private const double TargetAreaA = 9.1082396073229965070;
+
+        [Test]
+        public void TestIntegratePortal()
+        {
+            Assert.AreApproximatelyEqual(
+                TargetAreaA,
+                Integrate.OnClosedInterval(TargetFunctionA, StartA, StopA),
+                1e-5,
+                "Basic");
+
+            Assert.AreApproximatelyEqual(
+                TargetAreaA,
+                Integrate.OnClosedInterval(TargetFunctionA, StartA, StopA, 1e-10),
+                1e-10,
+                "Basic Target 1e-10");
+        }
+
+        [Test]
+        [Row(1e-5)]
+        [Row(1e-13)]
+        public void TestDoubleExponentialTransformationAlgorithm(double targetRelativeError)
+        {
+            var algorithm = new DoubleExponentialTransformation();
+
+            Assert.AreApproximatelyEqual(
+                TargetAreaA,
+                algorithm.Integrate(TargetFunctionA, StartA, StopA, targetRelativeError),
+                targetRelativeError * TargetAreaA,
+                "DET Adaptive {0}", targetRelativeError);
+        }
+
+        [Test]
+        public void TrapeziumRuleSupportsTwoPointIntegration()
+        {
+            var algorithm = new NewtonCotesTrapeziumRule();
+
+            Assert.AreApproximatelyEqual(
+                TargetAreaA,
+                algorithm.IntegrateTwoPoint(TargetFunctionA, StartA, StopA),
+                0.4 * TargetAreaA,
+                "Direct (1 Partition)");
+        }
+
+        [Test]
+        [Row(1, 3.5e-1)]
+        [Row(5, 1e-1)]
+        [Row(10, 2e-2)]
+        [Row(50, 6e-4)]
+        [Row(1000, 1.5e-6)]
+        public void TrapeziumRuleSupportsCompositeIntegration(int partitions, double maxRelativeError)
+        {
+            var algorithm = new NewtonCotesTrapeziumRule();
+
+            Assert.AreApproximatelyEqual(
+                TargetAreaA,
+                algorithm.IntegrateComposite(TargetFunctionA, StartA, StopA, partitions),
+                maxRelativeError * TargetAreaA,
+                "Composite {0} Partitions", partitions);
+        }
+
+        [Test]
+        [Row(1e-1)]
+        [Row(1e-5)]
+        [Row(1e-10)]
+        public void TrapeziumRuleSupportsAdaptiveIntegration(double targetRelativeError)
+        {
+            var algorithm = new NewtonCotesTrapeziumRule();
+
+            Assert.AreApproximatelyEqual(
+                TargetAreaA,
+                algorithm.IntegrateAdaptive(TargetFunctionA, StartA, StopA, targetRelativeError),
+                targetRelativeError * TargetAreaA,
+                "Adaptive {0}", targetRelativeError);
+        }
+
+        [Test]
+        public void SimpsonRuleSupportsThreePointIntegration()
+        {
+            var algorithm = new SimpsonRule();
+
+            Assert.AreApproximatelyEqual(
+                TargetAreaA,
+                algorithm.IntegrateThreePoint(TargetFunctionA, StartA, StopA),
+                0.2 * TargetAreaA,
+                "Direct (2 Partitions)");
+        }
+
+        [Test]
+        [Row(2, 1.7e-1)]
+        [Row(6, 1.2e-1)]
+        [Row(10, 8e-3)]
+        [Row(50, 8e-6)]
+        [Row(1000, 5e-11)]
+        public void SimpsonRuleSupportsCompositeIntegration(int partitions, double maxRelativeError)
+        {
+            var algorithm = new SimpsonRule();
+
+            Assert.AreApproximatelyEqual(
+                TargetAreaA,
+                algorithm.IntegrateComposite(TargetFunctionA, StartA, StopA, partitions),
+                maxRelativeError * TargetAreaA,
+                "Composite {0} Partitions", partitions);
+        }
+    }
+}
---
+++ src/Managed/Integration/Algorithms/SimpsonRule.cs
@@ -0,0 +1,97 @@
+﻿// <copyright file="SimpsonRule.cs" company="Math.NET">
+// Math.NET Numerics, part of the Math.NET Project
+// http://mathnet.opensourcedotnet.info
+//
+// Copyright (c) 2009 Math.NET
+//
+// Permission is hereby granted, free of charge, to any person
+// obtaining a copy of this software and associated documentation
+// files (the "Software"), to deal in the Software without
+// restriction, including without limitation the rights to use,
+// copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following
+// conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+// </copyright>
+
+namespace MathNet.Numerics.Integration.Algorithms
+{
+    using System;
+    using NumberTheory;
+    using Properties;
+
+    /// <summary>
+    /// Approximation algorithm for definite integrals by Simpson's rule.
+    /// </summary>
+    public class SimpsonRule
+    {
+        /// <summary>
+        /// Direct 3-point approximation of the definite integral in the provided interval by Simpson's rule.
+        /// </summary>
+        /// <param name="f">The analytic smooth function to integrate.</param>
+        /// <param name="intervalBegin">Where the interval starts, inclusive and finite.</param>
+        /// <param name="intervalEnd">Where the interval stops, inclusive and finite.</param>
+        /// <returns>approximation of the area in the given interval.</returns>
+        public double IntegrateThreePoint(
+            Func<double, double> f,
+            double intervalBegin,
+            double intervalEnd)
+        {
+            double midpoint = (intervalEnd + intervalBegin) / 2;
+            return (intervalEnd - intervalBegin) / 6 * (f(intervalBegin) + f(intervalEnd) + (4 * f(midpoint)));
+        }
+
+        /// <summary>
+        /// Composite N-point approximation of the definite integral in the provided interval by Simpson's rule.
+        /// </summary>
+        /// <param name="f">The analytic smooth function to integrate.</param>
+        /// <param name="intervalBegin">Where the interval starts, inclusive and finite.</param>
+        /// <param name="intervalEnd">Where the interval stops, inclusive and finite.</param>
+        /// <param name="numberOfPartitions">Even number of composite subdivision partitions.</param>
+        /// <returns>approximation of the area in the given interval.</returns>
+        public double IntegrateComposite(
+            Func<double, double> f,
+            double intervalBegin,
+            double intervalEnd,
+            int numberOfPartitions)
+        {
+            if (numberOfPartitions <= 0)
+            {
+                throw new ArgumentOutOfRangeException("numberOfPartitions", Resources.ArgumentPositive);
+            }
+
+            if (numberOfPartitions.IsOdd())
+            {
+                throw new ArgumentException(Resources.ArgumentEven, "numberOfPartitions");
+            }
+
+            double step = (intervalEnd - intervalBegin) / numberOfPartitions;
+            double factor = step / 3;
+
+            double offset = step;
+            int m = 4;
+            double sum = f(intervalBegin) + f(intervalEnd);
+            for (int i = 0; i < numberOfPartitions - 1; i++)
+            {
+                // NOTE (ruegg, 2009-01-07): Do not combine intervalBegin and offset (numerical stability)
+                sum += m * f(intervalBegin + offset);
+                m = 6 - m;
+                offset += step;
+            }
+
+            return factor * sum;
+        }
+    }
+}
