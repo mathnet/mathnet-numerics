11cfdde911f7ac6a8fc802b7832f5f308910a525
Jurgen Van Gael <jurgen.vangael@gmail.com>
2009-08-16 17:12:23 +0800

    Fixed some F# interface issues.
    Signed-off-by: jvangael <jurgen.vangael@gmail.com>

--- src/FSharpExamples/DenseVector.fs
+++ src/FSharpExamples/DenseVector.fs
@@ -2,10 +2,7 @@
 open MathNet.Numerics.LinearAlgebra
 
 /// Create a new 100 dimensional dense vector.
-let v = DenseVector.init 100 (fun i -> float i / 100.0)
+let v = Double.DenseVector.init 100 (fun i -> float i / 100.0)
 
 /// Another way to create a 100 dimensional dense vector is as follows.
 let w = vector (List.init 100 (fun i -> float i ** 2.0))
-
-/// Perform some binary arithmetic on vectors.
-let x = v + w
