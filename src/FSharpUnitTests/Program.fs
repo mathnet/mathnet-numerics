83355ad1c4b9257450cff383d5d7adfb1db0adac
Jurgen Van Gael <jurgen.vangael@gmail.com>
2009-08-19 15:24:18 +0800

    Fix bug in F# unit tests.
    Signed-off-by: jvangael <jurgen.vangael@gmail.com>

--- src/FSharpUnitTests/Program.fs
+++ src/FSharpUnitTests/Program.fs
@@ -13,17 +13,17 @@ let DenseVectorTests =
     
     specs "DenseVector" [
         spec "DenseVector.init"
-            (Double.DenseVector.init 100 (fun i -> float i / 100.0) |> should equal largev)
+            (Double.DenseVector.init 100 (fun i -> float i / 100.0) |> should approximately_vector_equal 16 largev)
         spec "DenseVector.of_list"
-            (Double.DenseVector.of_list [ for i in 0 .. 99 -> float i / 100.0 ] |> should equal largev)
+            (Double.DenseVector.of_list [ for i in 0 .. 99 -> float i / 100.0 ] |> should approximately_vector_equal 16 largev)
         spec "DenseVector.of_seq"
-            (Double.DenseVector.of_seq (seq { for i in 0 .. 99 -> float i / 100.0 }) |> should equal largev)
+            (Double.DenseVector.of_seq (seq { for i in 0 .. 99 -> float i / 100.0 }) |> should approximately_vector_equal 16 largev)
         spec "DenseVector.rangef"
             (Double.DenseVector.rangef 0.0 0.01 0.99
-             |> should equal (new Double.DenseVector( [| for i in 0 .. 99 -> 0.01 * float i |] ) ))
+             |> should approximately_vector_equal 16 (new Double.DenseVector( [| for i in 0 .. 99 -> 0.01 * float i |] ) ))
         spec "DenseVector.range"
             (Double.DenseVector.range 0 99
-             |> should equal (new Double.DenseVector( [| for i in 0 .. 99 -> float i |] ) ))
+             |> should approximately_vector_equal 16 (new Double.DenseVector( [| for i in 0 .. 99 -> float i |] ) ))
     ]
     
 /// Report on errors and success and exit.
