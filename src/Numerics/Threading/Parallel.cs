03899938d3fecd388e53ff866a10984d54234751
Marcus Cuda <marcus@cuda.net>
2009-08-21 21:12:37 +0800

    Parallel: added zero check, but aggregates still need fast forwards
    Signed-off-by: Marcus Cuda <marcus@cuda.net>

--- src/Numerics/Threading/Parallel.cs
+++ src/Numerics/Threading/Parallel.cs
@@ -128,6 +128,16 @@ namespace MathNet.Numerics.Threading
             var tasks = new Task<T>[ThreadQueue.ThreadCount];
             var size = count / tasks.Length;
 
+            if (count <= 1)
+            {
+//                if (count == 1)
+  //              {
+             //       body(fromInclusive);
+    //            }
+
+      //          return;
+            }
+
             var intial = localInit();
 
             // partition the jobs into separate sets for each but the last worked thread
