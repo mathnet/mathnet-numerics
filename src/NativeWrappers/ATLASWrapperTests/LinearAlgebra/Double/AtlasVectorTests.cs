98e5243f9e1b44cef93ff4de895a361260ff72df
Marcus Cuda <marcus@cuda.net>
2009-11-05 17:42:55 +0800

    setup native wrapper testing framework

---
+++ src/NativeWrappers/ATLASWrapperTests/LinearAlgebra/Double/AtlasVectorTests.cs
@@ -0,0 +1,14 @@
+﻿namespace MathNet.Numerics.ATLASWrapperTests.LinearAlgebra.Double
+{
+    using MbUnit.Framework;
+    using Numerics.LinearAlgebra.Double;
+
+    public class AtlasVectorTests : NativeVectorTests
+    {
+        [FixtureSetUp] 
+        public void SetUpProvider()
+        {
+            Control.LinearAlgebraProvider  = new Algorithms.LinearAlgebra.Atlas.AtlasLinearAlgebraProvider();
+        }
+    }
+}
---
+++ src/NativeWrappers/Common/LinearAlgebra/Double/NativeVectorTests.cs
@@ -0,0 +1,23 @@
+﻿namespace MathNet.Numerics.LinearAlgebra.Double
+{
+    using System.Collections.Generic;
+
+    public abstract class NativeVectorTests : UnitTests.LinearAlgebraTests.Double.VectorTests
+    {
+        protected override Vector CreateVector(int size)
+        {
+            return new DenseVector(size);
+        }
+
+        protected override Vector CreateVector(IList<double> data)
+        {
+            var vector = new DenseVector(data.Count);
+            for (var index = 0; index < data.Count; index++)
+            {
+                vector[index] = data[index];
+            }
+
+            return vector;
+        }
+    }
+}
