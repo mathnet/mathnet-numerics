c8fc867911cac8024d03a9e619bb02eea8a7633a
Marcus Cuda <marcus@cuda.net>
2009-11-06 20:44:40 +0800

    added x_scale routine and correted style cop errors

--- src/NativeWrappers/Common/blas.c
+++ src/NativeWrappers/Common/blas.c
@@ -21,3 +21,20 @@ DLLEXPORT void c_axpy( const int n, const Complex8 alpha, const Complex8 x[], Co
 DLLEXPORT void z_axpy( const int n, const Complex16 alpha, const Complex16 x[], Complex16 y[]){
 	cblas_zaxpy(n, &alpha, x, 1, y, 1);
 }
+
+DLLEXPORT void s_scale(const int n, const float alpha, float x[]){
+	cblas_sscal(n, alpha, x, 1);
+}
+
+DLLEXPORT void d_scale(const int n, const double alpha, double x[]){
+	cblas_dscal(n, alpha, x, 1);
+}
+
+DLLEXPORT void c_scale(const int n, const Complex8 alpha, Complex8 x[]){
+	cblas_cscal(n, &alpha, x, 1);
+}
+	
+DLLEXPORT void z_scale(const int n, const Complex16 alpha, Complex16 x[]){
+	cblas_zscal(n, &alpha, x, 1);
+}
+
