47c11a021c17265b1f74cfb3e4c785ad2bfd6634
Christoph Ruegg <git@cdrnet.ch>
2010-10-24 13:46:11 +0200

    CI: TeamCity: Preparation build step now using T4 v10.0 instead of v1.2

--- build/ContinuousIntegration/TeamCity/prepare-t4.bat
+++ build/ContinuousIntegration/TeamCity/prepare-t4.bat
@@ -9,12 +9,12 @@ SET common=%CommonProgramFiles(x86)%
 GOTO prepare
 
 :prepare
-"%common%\Microsoft Shared\TextTemplating\1.2\texttransform.exe" -out ..\..\..\src\Numerics\Version.cs -P "%ProgramFiles%\Reference Assemblies\Microsoft\Framework\v3.5" ..\..\..\src\Numerics\Version.tt
-"%common%\Microsoft Shared\TextTemplating\1.2\texttransform.exe" -out ..\..\..\src\Silverlight\Version.cs -P "%ProgramFiles%\Reference Assemblies\Microsoft\Framework\v3.5" ..\..\..\src\Silverlight\Version.tt
+"%common%\Microsoft Shared\TextTemplating\10.0\texttransform.exe" -out ..\..\..\src\Numerics\Version.cs -P "%ProgramFiles%\Reference Assemblies\Microsoft\Framework\v3.5" ..\..\..\src\Numerics\Version.tt
+"%common%\Microsoft Shared\TextTemplating\10.0\texttransform.exe" -out ..\..\..\src\Silverlight\Version.cs -P "%ProgramFiles%\Reference Assemblies\Microsoft\Framework\v3.5" ..\..\..\src\Silverlight\Version.tt
 
-"%common%\Microsoft Shared\TextTemplating\1.2\texttransform.exe" -out ..\..\..\src\Numerics\Algorithms\LinearAlgebra\Atlas\AtlasLinearAlgebraProvider.cs -P "%ProgramFiles%\Reference Assemblies\Microsoft\Framework\v3.5" ..\..\..\src\Numerics\Algorithms\LinearAlgebra\Atlas\AtlasLinearAlgebraProvider.tt
-"%common%\Microsoft Shared\TextTemplating\1.2\texttransform.exe" -out ..\..\..\src\Numerics\Algorithms\LinearAlgebra\Atlas\SafeNativeMethods.cs -P "%ProgramFiles%\Reference Assemblies\Microsoft\Framework\v3.5" ..\..\..\src\Numerics\Algorithms\LinearAlgebra\Atlas\SafeNativeMethods.tt
-"%common%\Microsoft Shared\TextTemplating\1.2\texttransform.exe" -out ..\..\..\src\Numerics\Algorithms\LinearAlgebra\Mkl\MklLinearAlgebraProvider.cs -P "%ProgramFiles%\Reference Assemblies\Microsoft\Framework\v3.5" ..\..\..\src\Numerics\Algorithms\LinearAlgebra\Mkl\MklLinearAlgebraProvider.tt
-"%common%\Microsoft Shared\TextTemplating\1.2\texttransform.exe" -out ..\..\..\src\Numerics\Algorithms\LinearAlgebra\Mkl\SafeNativeMethods.cs -P "%ProgramFiles%\Reference Assemblies\Microsoft\Framework\v3.5" ..\..\..\src\Numerics\Algorithms\LinearAlgebra\Mkl\SafeNativeMethods.tt
+"%common%\Microsoft Shared\TextTemplating\10.0\texttransform.exe" -out ..\..\..\src\Numerics\Algorithms\LinearAlgebra\Atlas\AtlasLinearAlgebraProvider.cs -P "%ProgramFiles%\Reference Assemblies\Microsoft\Framework\v3.5" ..\..\..\src\Numerics\Algorithms\LinearAlgebra\Atlas\AtlasLinearAlgebraProvider.tt
+"%common%\Microsoft Shared\TextTemplating\10.0\texttransform.exe" -out ..\..\..\src\Numerics\Algorithms\LinearAlgebra\Atlas\SafeNativeMethods.cs -P "%ProgramFiles%\Reference Assemblies\Microsoft\Framework\v3.5" ..\..\..\src\Numerics\Algorithms\LinearAlgebra\Atlas\SafeNativeMethods.tt
+"%common%\Microsoft Shared\TextTemplating\10.0\texttransform.exe" -out ..\..\..\src\Numerics\Algorithms\LinearAlgebra\Mkl\MklLinearAlgebraProvider.cs -P "%ProgramFiles%\Reference Assemblies\Microsoft\Framework\v3.5" ..\..\..\src\Numerics\Algorithms\LinearAlgebra\Mkl\MklLinearAlgebraProvider.tt
+"%common%\Microsoft Shared\TextTemplating\10.0\texttransform.exe" -out ..\..\..\src\Numerics\Algorithms\LinearAlgebra\Mkl\SafeNativeMethods.cs -P "%ProgramFiles%\Reference Assemblies\Microsoft\Framework\v3.5" ..\..\..\src\Numerics\Algorithms\LinearAlgebra\Mkl\SafeNativeMethods.tt
 
 set common= 
