f2d3a5291b6633c9553f785460a1bd78f7fe9763
Alexander Karatarakis <danimothonline@gmail.com>
2011-03-08 19:00:23 +0200

    Implemented DoTransposeThisAndMultiply for matrices on Double.

--- src/Numerics/LinearAlgebra/Double/Matrix.cs
+++ src/Numerics/LinearAlgebra/Double/Matrix.cs
@@ -270,6 +270,28 @@ namespace MathNet.Numerics.LinearAlgebra.Double
         }
 
         /// <summary>
+        /// Multiplies the transpose of this matrix with another matrix and places the results into the result matrix.
+        /// </summary>
+        /// <param name="other">The matrix to multiply with.</param>
+        /// <param name="result">The result of the multiplication.</param>
+        protected override void DoTransposeThisAndMultiply(Matrix<double> other, Matrix<double> result)
+        {
+            for (var j = 0; j < other.ColumnCount; j++)
+            {
+                for (var i = 0; i < ColumnCount; i++)
+                {
+                    var s = 0.0;
+                    for (var l = 0; l < RowCount; l++)
+                    {
+                        s += At(l, i) * other.At(l, j);
+                    }
+
+                    result.At(i, j, s);
+                }
+            }
+        }
+
+        /// <summary>
         /// Multiplies the transpose of this matrix with a vector and places the results into the result vector.
         /// </summary>
         /// <param name="rightSide">The vector to multiply with.</param>
