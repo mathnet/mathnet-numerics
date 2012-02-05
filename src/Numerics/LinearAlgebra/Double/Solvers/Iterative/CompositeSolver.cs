9aea21aa8820466c4b44fd7f8238797435f83eee
cureos <anders@cureos.com>
2012-02-05 21:54:18 +0100

    Assembly.Load in CompositeSolver classes use the assembly name string to avoid build errors in Silverlight 5. Fixes issue #27 in mathnet fork

--- src/Numerics/LinearAlgebra/Double/Solvers/Iterative/CompositeSolver.cs
+++ src/Numerics/LinearAlgebra/Double/Solvers/Iterative/CompositeSolver.cs
@@ -156,7 +156,7 @@ namespace MathNet.Numerics.LinearAlgebra.Double.Solvers.Iterative
 
             // Now load the assembly with an AssemblyName
             var assemblyName = new AssemblyName(assemblyFileName);
-            var assembly = Assembly.Load(assemblyName);
+            var assembly = Assembly.Load(assemblyName.FullName);
             
             // <ay throws:
             // ArgumentNullException --> Can't get this because we checked that the file exists.
@@ -188,7 +188,7 @@ namespace MathNet.Numerics.LinearAlgebra.Double.Solvers.Iterative
                 throw new ArgumentNullException("assemblyName");
             }
 
-            var assembly = Assembly.Load(assemblyName);
+            var assembly = Assembly.Load(assemblyName.FullName);
 
             // May throw:
             // ArgumentNullException --> Can't get this because we checked it.
