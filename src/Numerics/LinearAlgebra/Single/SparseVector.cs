0c1b688a62b2a1371fc6863d90f4883f7b96f1cd
Marcus Cuda <marcus@cuda.org>
2012-10-22 14:34:25 +0300

    added the 'optimized' version of Multiply(scale) from Double.SparseVector to the other three SparseVectors

--- src/Numerics/LinearAlgebra/Single/SparseVector.cs
+++ src/Numerics/LinearAlgebra/Single/SparseVector.cs
@@ -689,6 +689,28 @@ namespace MathNet.Numerics.LinearAlgebra.Single
         }
 
         /// <summary>
+        /// Multiplies a scalar to each element of the vector.
+        /// </summary>
+        /// <param name="scalar">The scalar to multiply.</param>
+        /// <returns>A new vector that is the multiplication of the vector and the scalar.</returns>
+        public override Vector<float> Multiply(float scalar)
+        {
+            if (scalar == 1.0f)
+            {
+                return Clone();
+            }
+
+            if (scalar == 0f)
+            {
+                return new SparseVector(Count);
+            }
+
+            var copy = new SparseVector(this);
+            Control.LinearAlgebraProvider.ScaleArray(scalar, copy._nonZeroValues, copy._nonZeroValues);
+            return copy;
+        }
+
+        /// <summary>
         /// Multiplies a scalar to each element of the vector and stores the result in the result vector.
         /// </summary>
         /// <param name="scalar">
