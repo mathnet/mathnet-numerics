e774bf4be4b2ded5838d383d09b78b043c0c4e33
Alexander Karatarakis <alex@karatarakis.com>
2012-11-21 17:32:48 +0200

    Add new abstract class: SquareMatrix
    Signed-off-by: Alexander Karatarakis <alex@karatarakis.com>

---
+++ src/Numerics/LinearAlgebra/Generic/SquareMatrix.cs
@@ -0,0 +1,39 @@
+﻿namespace MathNet.Numerics.LinearAlgebra.Generic
+{
+    using System;
+
+    using MathNet.Numerics.LinearAlgebra.Storage;
+
+    using Properties;
+
+    /// <summary>
+    /// Abstract class for square matrices. 
+    /// </summary>
+    /// <typeparam name="T">Supported data types are <c>double</c>, <c>single</c>, <see cref="Complex"/>, and <see cref="Complex32"/>.</typeparam>
+    [Serializable]
+    public abstract class SquareMatrix<T> : Matrix<T>
+         where T : struct, IEquatable<T>, IFormattable
+    {
+        /// <summary>
+        ///   Number of rows or columns.
+        /// </summary>
+        protected readonly int Order;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SquareMatrix{T}"/> class. 
+        /// </summary>
+        /// <exception cref="ArgumentException">
+        /// If the matrix is not square.
+        /// </exception>
+        protected SquareMatrix(MatrixStorage<T> storage)
+            : base(storage)
+        {
+            if (storage.RowCount != storage.ColumnCount)
+            {
+                throw new ArgumentException(Resources.ArgumentMatrixSquare);
+            }
+
+            Order = storage.RowCount;
+        }
+    }
+}
