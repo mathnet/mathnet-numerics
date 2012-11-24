7cebb6ea79d10c8147d39c172bacba856442244a
Alexander Karatarakis <alex@karatarakis.com>
2012-11-24 19:16:40 +0200

    Add SymmetricDenseMatrixTests
    Signed-off-by: Alexander Karatarakis <alex@karatarakis.com>

---
+++ src/UnitTests/LinearAlgebraTests/Double/SymmetricDenseMatrixTests.cs
@@ -0,0 +1,99 @@
+﻿// <copyright file="SymmetricDenseMatrixTests.cs" company="Math.NET">
+// Math.NET Numerics, part of the Math.NET Project
+// http://numerics.mathdotnet.com
+// http://github.com/mathnet/mathnet-numerics
+// http://mathnetnumerics.codeplex.com
+// Copyright (c) 2009-2010 Math.NET
+// Permission is hereby granted, free of charge, to any person
+// obtaining a copy of this software and associated documentation
+// files (the "Software"), to deal in the Software without
+// restriction, including without limitation the rights to use,
+// copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following
+// conditions:
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+// </copyright>
+
+namespace MathNet.Numerics.UnitTests.LinearAlgebraTests.Double
+{
+    using MathNet.Numerics.LinearAlgebra.Double;
+
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Symmetric Dense matrix tests.
+    /// </summary>
+    public class SymmetricDenseMatrixTests : SymmetricMatrixTests
+    {
+        /// <summary>
+        /// Creates a matrix for the given number of rows and columns.
+        /// </summary>
+        /// <param name="rows">
+        /// The number of rows.
+        /// </param>
+        /// <param name="columns">
+        /// The number of columns.
+        /// </param>
+        /// <returns>
+        /// A matrix with the given dimensions.
+        /// </returns>
+        protected override Matrix CreateMatrix(int rows, int columns)
+        {
+            return new DenseMatrix(rows, columns);
+        }
+
+        /// <summary>
+        /// Creates a matrix from a 2D array.
+        /// </summary>
+        /// <param name="data">
+        /// The 2D array to create this matrix from.
+        /// </param>
+        /// <returns>
+        /// A matrix with the given values.
+        /// </returns>
+        protected override Matrix CreateMatrix(double[,] data)
+        {
+            return SymmetricMatrix.CheckIfSymmetric(data)
+                       ? (Matrix)new SymmetricDenseMatrix(data)
+                       : new DenseMatrix(data);
+        }
+
+        /// <summary>
+        /// Creates a vector of the given size.
+        /// </summary>
+        /// <param name="size">
+        /// The size of the vector to create.
+        /// </param>
+        /// <returns>
+        /// The new vector. 
+        /// </returns>
+        protected override Vector CreateVector(int size)
+        {
+            return new DenseVector(size);
+        }
+
+        /// <summary>
+        /// Creates a vector from an array.
+        /// </summary>
+        /// <param name="data">
+        /// The array to create this vector from.
+        /// </param>
+        /// <returns>
+        /// The new vector. 
+        /// </returns>
+        protected override Vector CreateVector(double[] data)
+        {
+            return new DenseVector(data);
+        }
+    }
+}
