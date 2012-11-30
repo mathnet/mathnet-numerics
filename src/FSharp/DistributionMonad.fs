94677c65486659824b7cc5553cff69b9b85a08dd
Gustavo Guerra <gustavo@codebeside.org>
2012-11-30 22:28:44 +0000

    Bring Complex & BigRational from F# PowerPack, and DistributionMonad from FSharpx

---
+++ src/FSharp/DistributionMonad.fs
@@ -0,0 +1,98 @@
+﻿namespace MathNet.Numerics
+
+#nowarn "40"
+
+open System
+open System.Collections
+open System.Collections.Generic
+
+module Distribution =
+    
+    type 'a Outcome = {
+        Value: 'a
+        Probability : BigRational    }
+    
+    type 'a Distribution = 'a Outcome seq
+    
+    // P(A AND B) = P(A | B) * P(B)
+    let bind (f: 'a -> 'b Distribution) (dist:'a Distribution) =
+        dist 
+        |> Seq.map (fun p1 -> 
+            f p1.Value
+            |> Seq.map (fun p2 -> 
+                { Value = p2.Value; 
+                    Probability = 
+                        p1.Probability * p2.Probability}))
+        |> Seq.concat : 'b Distribution
+    
+    /// Sequentially compose two actions, passing any value produced by the first as an argument to the second.
+    let inline (>>=) dist f = bind f dist
+    /// Flipped >>=
+    let inline (=<<) f dist = bind f dist
+    
+    /// Inject a value into the Distribution type
+    let returnM (value:'a) =     
+        Seq.singleton { Value = value ; Probability = 1N/1N }
+            : 'a Distribution
+    
+    type DistributionMonadBuilder() =
+        member this.Bind (r, f) = bind f r
+        member this.Return x = returnM x
+        member this.ReturnFrom x = x
+    
+    let distribution = DistributionMonadBuilder()
+    
+    // Create some helpers
+    let toUniformDistribution seq : 'a Distribution =
+        let l = Seq.length seq
+        seq 
+        |> Seq.map (fun e ->
+            { Value = e; 
+                Probability = 1N / bignum.FromInt l })
+    
+    let probability (dist:'a Distribution) = 
+        dist
+        |> Seq.map (fun o -> o.Probability)
+        |> Seq.sum
+    
+    let certainly = returnM
+    let impossible<'a> :'a Distribution = toUniformDistribution []
+    
+    let fairDice sides = toUniformDistribution [1..sides]
+    
+    type CoinSide = 
+        | Heads 
+        | Tails
+    
+    let fairCoin = toUniformDistribution [Heads; Tails]
+    
+    let filter predicate (dist:'a Distribution) : 'a Distribution =
+        dist |> Seq.filter (fun o -> predicate o.Value)
+    
+    let filterInAnyOrder items dist =
+        items
+        |> Seq.fold (fun d item -> filter (Seq.exists ((=) (item))) d) dist
+
+    /// Transforms a Distribution value by using a specified mapping function.
+    let map f (dist:'a Distribution) : 'b Distribution = 
+        dist 
+        |> Seq.map (fun o -> { Value = f o.Value; Probability = o.Probability })
+    
+    let selectOne values =
+        [for e in values -> e,values |> Seq.filter ((<>) e)] 
+        |> toUniformDistribution
+    
+    let rec selectMany n values =
+        match n with 
+        | 0 -> certainly ([],values)
+        | _ -> 
+            distribution {
+                let! (x,c1) = selectOne values
+                let! (xs,c2) = selectMany (n-1) c1
+                return x::xs,c2}
+            
+    let select n values = 
+        selectMany n values     
+        |> map (fst >> List.rev)
+    
+    let remove items = Seq.filter (fun v -> Seq.forall ((<>) v) items)
---
+++ src/FSharpUnitTests/DistributionTest.fs
@@ -0,0 +1,79 @@
+﻿module MathNet.Numerics.Tests.DistributionTest
+
+open MathNet.Numerics
+open MathNet.Numerics.Distribution
+open NUnit.Framework
+open FsUnit
+
+[<Test>]
+let ``When creating a empty distribution, then the probability should be 1``() =
+  let actual = distribution { return () }
+  probability actual |> should equal (1N/1N)
+
+let sumOfTwoFairDices = distribution {
+  let! d1 = fairDice 6
+  let! d2 = fairDice 6
+  return d1 + d2 }
+
+[<Test>]
+let ``When creating two fair dices, then P(Sum of 2 dices = 7) should be 1/6``() =
+  sumOfTwoFairDices 
+    |> filter ((=) 7)
+    |> probability 
+    |> should equal (1N/6N)
+
+let fairCoinAndDice = distribution {
+  let! d = fairDice 6
+  let! c = fairCoin
+  return d,c }
+
+[<Test>]
+let ``When creating a fair coin and a fair dice, then P(Heads) should be 1/2``() =
+  fairCoinAndDice 
+    |> filter (fun (_,c) -> c = Heads)
+    |> probability 
+    |> should equal (1N/2N)
+
+[<Test>]
+let ``When creating a fair coin and a fair dice, then P(Heads and dice > 3) should be 1/4``() =
+  fairCoinAndDice 
+    |> filter (fun (d,c) -> c = Heads && d > 3)
+    |> probability 
+    |> should equal (1N/4N)
+
+// MontyHall Problem
+// See Martin Erwig and Steve Kollmansberger's paper 
+// "Functional Pearls: Probabilistic functional programming in Haskell"
+
+type Outcome = 
+| Car
+| Goat
+
+let firstChoice = toUniformDistribution [Car; Goat; Goat]
+
+let switch firstCoice =
+    match firstCoice with
+    | Car -> 
+        // If you had the car and you switch ==> you lose since there are only goats left
+        certainly Goat 
+    | Goat -> 
+        // If you had the goat, the host has to take out another goat ==> you win
+        certainly Car 
+ 
+[<Test>]
+let ``When making the first choice in a MontyHall situation, the chances to win should be 1/3``() =
+  firstChoice 
+    |> filter ((=) Car)
+    |> probability 
+    |> should equal (1N/3N)
+
+let montyHallWithSwitch = distribution {
+    let! firstDoor = firstChoice
+    return! switch firstDoor }
+
+[<Test>]
+let ``When switching in a MontyHall situation, the chances to win should be 2/3``() =
+  montyHallWithSwitch
+    |> filter ((=) Car)
+    |> probability 
+    |> should equal (2N/3N)
