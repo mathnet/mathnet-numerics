5dae5505c0a041bf68bc6839262d33b8ac86fb70
Scott Stephens <stephens.js@gmail.com>
2013-01-31 09:23:50 -0600

    First commit working on optimization tools

---
+++ src/Numerics/Optimization/GoldenSectionMinimizer.cs
@@ -0,0 +1,6 @@
+﻿namespace MathNet.Numerics.Optimization
+{
+    class GoldenSectionMinimizer
+    {
+    }
+}
---
+++ src/Numerics/Optimization/LineSearch/StrongWolfeLineSearch.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MathNet.Numerics.Optimization
+{
+    class StrongWolfeLineSearch
+    {
+    }
+}
