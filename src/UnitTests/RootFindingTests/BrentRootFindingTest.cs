a177228238eb5113fa04dcd7d70e513ece402808
Christoph Ruegg <git@cdrnet.ch>
2013-05-01 17:47:14 +0200

    RootFinding: simplify unit test

---
+++ src/UnitTests/RootFindingTests/BrentRootFindingTest.cs
@@ -0,0 +1,17 @@
+﻿using MathNet.Numerics.RootFinding;
+using NUnit.Framework;
+
+namespace MathNet.Numerics.UnitTests.RootFindingTests
+{
+    [TestFixture]
+    public class BrentRootFindingTest
+    {
+        [Test]
+        public void MultipleRoots()
+        {
+            var solver = new BrentRootFinder(100, 1e-14) {Func = x => x*x - 4};
+            double root = solver.Solve(-5, 5);
+            Assert.AreEqual(0, solver.Func(root));
+        }
+    }
+}
