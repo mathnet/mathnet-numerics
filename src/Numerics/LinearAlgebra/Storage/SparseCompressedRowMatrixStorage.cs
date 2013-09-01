d761742ac9bb30934e08f60752617f53e5b305b9
Christoph Ruegg <git@cdrnet.ch>
2013-09-01 14:18:46 +0200

    LA: Drop bad using-namespace (and fix portable build)

--- src/Numerics/LinearAlgebra/Storage/SparseCompressedRowMatrixStorage.cs
+++ src/Numerics/LinearAlgebra/Storage/SparseCompressedRowMatrixStorage.cs
@@ -31,7 +31,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.Cryptography.X509Certificates;
 using MathNet.Numerics.Properties;
 
 namespace MathNet.Numerics.LinearAlgebra.Storage
