a506145eff7271401a0d23802e947f8af4b9e699
Scott Stephens <stephens.js@gmail.com>
2013-10-15 19:43:20 -0500

    Optimization: Finish implementation of Golden Section minimum search

---
+++ src/Numerics/Optimization/MinimizationResult1D.cs
@@ -0,0 +1,17 @@
+﻿namespace MathNet.Numerics.Optimization
+{
+    public class MinimizationResult1D
+    {
+        public double MinimizingPoint { get { return FunctionInfoAtMinimum.Point; } }
+        public IEvaluation1D FunctionInfoAtMinimum { get; private set; }
+        public int Iterations { get; private set; }
+        public MinimizationResult.ExitCondition ReasonForExit { get; private set; }
+
+        public MinimizationResult1D(IEvaluation1D functionInfo, int iterations, MinimizationResult.ExitCondition reasonForExit)
+        {
+            FunctionInfoAtMinimum = functionInfo;
+            Iterations = iterations;
+            ReasonForExit = reasonForExit;
+        }
+    }
+}
