5e2fb1e9755d84bf74d933bf9fc7ab7ade016c1a
gauthier <gauthier@t420s.local>
2014-03-25 08:15:51 -0700

    fix build scripts to point to appropriate NuGet.exe on case sensitive file systems

--- build.cmd
+++ build.cmd
@@ -1,7 +1,7 @@
 @echo off
 cls
 if not exist packages\FAKE\tools\FAKE.exe (
-  .nuget\nuget.exe install FAKE -OutputDirectory packages -ExcludeVersion
+  .nuget\NuGet.exe install FAKE -OutputDirectory packages -ExcludeVersion
 )
 packages\FAKE\tools\FAKE.exe build.fsx %*
 pause
--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 if [ ! -f packages/FAKE/tools/FAKE.exe ]; then
-  mono .nuget/nuget.exe install FAKE -OutputDirectory packages -ExcludeVersion
+  mono .nuget/NuGet.exe install FAKE -OutputDirectory packages -ExcludeVersion
 fi
 mono packages/FAKE/tools/FAKE.exe build.fsx $@
