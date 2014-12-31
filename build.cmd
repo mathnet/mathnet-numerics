33a13832755d3fbafd8e01d1c33a1ab9539afdf8
Christoph Ruegg <git@cdrnet.ch>
2014-12-31 20:10:36 +0100

    Update dependencies/tooling

--- build.cmd
+++ build.cmd
@@ -3,5 +3,5 @@ cls
 if not exist tools\paket\paket.exe (
   tools\paket\paket.bootstrapper.exe
 )
-tools\paket\paket.exe install
+tools\paket\paket.exe restore
 packages\FAKE\tools\FAKE.exe build.fsx %*
--- build.sh
+++ build.sh
@@ -3,11 +3,11 @@ if test "$OS" = "Windows_NT"
 then
   # use .Net
   [ ! -f tools/paket/paket.exe ] && tools/paket/paket.bootstrapper.exe
-  tools/paket/paket.exe install
+  tools/paket/paket.exe restore
   packages/FAKE/tools/FAKE.exe build.fsx $@
 else
   # use mono
   [ ! -f tools/paket/paket.exe ] && mono --runtime=v4.0 tools/paket/paket.bootstrapper.exe
-  mono --runtime=v4.0 tools/paket/paket.exe install
+  mono --runtime=v4.0 tools/paket/paket.exe restore
   mono --runtime=v4.0 packages/FAKE/tools/FAKE.exe $@ --fsiargs -d:MONO build.fsx
 fi
