6a0475c1f951a6e58686385a5ba5dc245ed0c3b1
Christoph Ruegg <git@cdrnet.ch>
2015-01-03 21:20:27 +0100

    Build: updated vagrant bootstrap to use newest xamarin mono packages

--- vagrant-bootstrap.sh
+++ vagrant-bootstrap.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+echo "deb http://download.mono-project.com/repo/debian wheezy main" | sudo tee /etc/apt/sources.list.d/mono-xamarin.list
+
 apt-get update
+apt-get -y upgrade
+
 apt-get -y install mono-complete
 apt-get -y install mono-tools-devel
+apt-get -y install referenceassemblies-pcl
 apt-get -y install fsharp
+
+apt-get -y autoremove
+
 mozroots --import --sync
