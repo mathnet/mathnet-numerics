c88069aae0b181aeaf8d4c2f515d7e5f6fd465b2
unknown <FEisele@DE-NBK-1150.cellent.int>
2015-06-19 12:07:58 +0200

    Added atr indicator and sma indicator

---
+++ src/UnitTests/FinancialTests/IndicatorsTests.cs
@@ -0,0 +1,99 @@
+﻿// <copyright file="IndicatorsTests.cs" company="Math.NET">
+// Math.NET Numerics, part of the Math.NET Project
+// http://numerics.mathdotnet.com
+// http://github.com/mathnet/mathnet-numerics
+// http://mathnetnumerics.codeplex.com
+//
+// Copyright (c) 2009-2015 Math.NET
+//
+// Permission is hereby granted, free of charge, to any person
+// obtaining a copy of this software and associated documentation
+// files (the "Software"), to deal in the Software without
+// restriction, including without limitation the rights to use,
+// copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following
+// conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+// </copyright>
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MathNet.Numerics.Financial;
+using NUnit.Framework;
+
+namespace MathNet.Numerics.UnitTests.FinancialTests
+{
+    [TestFixture, Category("Financial")]
+    public class IndicatorsTests
+    {
+        [Test]
+        public void Atr_ShouldReturnValuesWithoutException()
+        {
+            var inputBars = GenerateValidBars();
+            var atr = inputBars.ATR(14);
+        }
+
+        [Test]
+        public void Atr_ShouldReturnValue()
+        {
+            var inputBars = GenerateValidBars();
+            var atr = inputBars.ATR(14);
+
+            var expectedValueAtBar20 = 132.33;
+            var actual = atr.ElementAt(20);
+            Assert.AreEqual(expectedValueAtBar20, actual);
+     
+        }
+
+        [Test]
+        public void Atr_ShouldRaiseException_IfPeriodIsZero()
+        {
+            var inputBars = GenerateValidBars();
+            var period = 0;
+
+            Assert.That(() => inputBars.ATR(period), Throws.Exception.TypeOf<ArgumentException>());
+        }
+
+        [Test]
+        public void Atr_ShouldRaiseException_IfPeriodIsGreaterThanBarCount()
+        {
+            var inputBars = GenerateValidBars();
+            inputBars = inputBars.Take(10);
+            var period = 11;
+
+            Assert.That(() => inputBars.ATR(period), Throws.Exception.TypeOf<ArgumentException>());
+        }
+
+
+        private IEnumerable<Bar> GenerateValidBars()
+        {
+            StockDataReader reader = new StockDataReader();
+            var data = reader.ReadFile("./data/Finance/DaxHistoricalData.dat");
+           
+            List<Bar> inputBars = new List<Bar>();
+
+            foreach (var stockData in data)
+            {
+                var bar = new Bar(stockData.High, stockData.Low, stockData.Open, stockData.Close);
+                inputBars.Add(bar);
+            }
+            return inputBars;
+        }
+    }
+
+}
---
+++ src/UnitTests/FinancialTests/StockDataReader.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathNet.Numerics.UnitTests.FinancialTests
+{
+    internal class StockDataReader
+    {
+
+        public IEnumerable<StockData> ReadFile(string filePath)
+        {
+            List<StockData> resultList = new List<StockData>();
+
+            using (var reader = new StreamReader(filePath))
+            {
+                var firstLine = reader.ReadLine();
+
+                string line = string.Empty;
+
+                while(true)
+                {
+                    line = reader.ReadLine();
+                    if (line == null)
+                        break;
+
+                    line = line.Trim();
+                    if (line.Equals(string.Empty))
+                        continue;
+
+                    var stringValues = line.Split(';');
+
+                    //first Datetime:
+                    DateTime date = DateTime.Parse(stringValues[0]);
+                    double open = double.Parse(stringValues[1], CultureInfo.InvariantCulture);
+                    double high = double.Parse(stringValues[2], CultureInfo.InvariantCulture);
+                    double low = double.Parse(stringValues[3], CultureInfo.InvariantCulture);
+                    double close = double.Parse(stringValues[4], CultureInfo.InvariantCulture);
+                    int volume = int.Parse(stringValues[5], CultureInfo.InvariantCulture);
+
+                    StockData stockData = new StockData(date, open, close, high, low, volume);
+                    resultList.Add(stockData);
+                }
+            }
+
+            return resultList;
+        }
+    }
+
+
+
+    internal class StockData
+    {
+        public StockData(DateTime dateTime,double open,double close, double high,double low,int volume)
+        {
+            Date = dateTime;
+            Open = open;
+            Close = close;
+            High = high;
+            Low = low;
+            Volume = volume;
+        }
+
+        public DateTime Date { get; private set; }
+        public double Open { get; private set; }
+        public double Close { get; private set; }
+        public double High { get; private set; }
+        public double Low { get; private set; }
+
+        public int Volume { get; private set; }
+    }
+
+
+}
+
