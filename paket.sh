53dc96e4f55518c35630a11bea55a693f1f8ebe8
Christoph Ruegg <git@cdrnet.ch>
2015-08-15 16:20:32 +0200

    Tooling: reinstall dependencies with current paket release

--- paket.sh
+++ paket.sh
@@ -2,10 +2,10 @@
 if test "$OS" = "Windows_NT"
 then
   # use .Net
-  [ ! -f tools/paket/paket.exe ] && tools/paket/paket.bootstrapper.exe
-  tools/paket/paket.exe $@
+  [ ! -f .paket/paket.exe ] && .paket/paket.bootstrapper.exe
+  .paket/paket.exe $@
 else
   # use mono
-  [ ! -f tools/paket/paket.exe ] && mono --runtime=v4.0 tools/paket/paket.bootstrapper.exe
-  mono --runtime=v4.0 tools/paket/paket.exe $@
+  [ ! -f .paket/paket.exe ] && mono --runtime=v4.0 .paket/paket.bootstrapper.exe
+  mono --runtime=v4.0 .paket/paket.exe $@
 fi
