8afcacc0d40abc76edcb09524128c872531cfa0d
MaLiN2223 <szyszka45@gmail.com>
2016-02-14 02:42:29 +0100

    Few tests and magnitude to norm name change

--- src/Numerics/Quaternion32.cs
+++ src/Numerics/Quaternion32.cs
@@ -31,6 +31,7 @@
 namespace MathNet.Numerics
 {
     using System;
+    using Distributions;
 
     /// <summary>
     /// 32-bit single precision Quaternion number class.
@@ -42,78 +43,121 @@ namespace MathNet.Numerics
     /// <c>*</c>, <c>/</c>, <c>==</c>, <c>!=</c> are defined in the
     /// canonical way. 
     /// </para>
+    /// <para>
+    /// Class is based on http://web.cs.iastate.edu/~cs577/handouts/quaternion.pdf and http://mathworld.wolfram.com/Quaternion.html
+    /// </para>
     /// </remarks>
     public struct Quaternion32 : IFormattable, IEquatable<Quaternion32>
     {
-        private float w, x, y, z;
+        private float a1, a2, a3, a4;
         #region constructors
-        public Quaternion32(float w, float x, float y, float z)
+        public Quaternion32(float a1, float a2, float a3, float a4)
         {
-            this.w = w;
-            this.x = x;
-            this.y = y;
-            this.z = z;
+            this.a1 = a1;
+            this.a2 = a2;
+            this.a3 = a3;
+            this.a4 = a4;
         }
 
         public Quaternion32(Complex32 a, Complex32 b)
         {
-            w = a.Real;
-            x = a.Imaginary;
-            y = b.Real;
-            z = b.Imaginary;
+            a1 = a.Real;
+            a2 = a.Imaginary;
+            a3 = b.Real;
+            a4 = b.Imaginary;
         }
         #endregion
         #region operators
         public static Quaternion32 operator +(Quaternion32 q1, Quaternion32 q2)
         {
-            return new Quaternion32(q1.w + q2.w, q1.x + q2.x, q1.y + q2.y, q1.z + q2.z);
+            return new Quaternion32(q1.a1 + q2.a1, q1.a2 + q2.a2, q1.a3 + q2.a3, q1.a4 + q2.a4);
+        }
+
+        public static Quaternion32 operator +(Quaternion32 q1, float f)
+        {
+            return new Quaternion32(q1.a1 + f, q1.a2, q1.a3, q1.a4);
+        }
+
+        public static Quaternion32 operator +(float f, Quaternion32 q1)
+        {
+            return q1 + f;
         }
         public static Quaternion32 operator -(Quaternion32 q1, Quaternion32 q2)
         {
-            return new Quaternion32(q1.w - q2.w, q1.x - q2.x, q1.y - q2.y, q1.z - q2.z);
+            return new Quaternion32(q1.a1 - q2.a1, q1.a2 - q2.a2, q1.a3 - q2.a3, q1.a4 - q2.a4);
         }
 
+        public static Quaternion32 operator -(Quaternion32 q1, float f)
+        {
+            return new Quaternion32(q1.a1 - f, q1.a2, q1.a3, q1.a4);
+        }
+
+        public static Quaternion32 operator -(float f, Quaternion32 q1)
+        {
+            return new Quaternion32(f - q1.a1, q1.a2, q1.a3, q1.a4);
+        }
         public static Quaternion32 operator *(Quaternion32 q1, Quaternion32 q2)
         {
             return new Quaternion32(
-                 q1.w * q2.w - q1.x * q2.x - q1.y * q2.y - q1.z * q2.z,
-                q1.w * q2.x + q1.x * q2.w - q1.y * q2.z + q1.z * q2.y,
-                q1.w * q2.y + q1.x * q2.z + q1.y * q2.w - q1.z * q2.x,
-                q1.w * q2.z - q1.x * q2.y + q1.y * q2.x + q1.z * q2.w
+                 q1.a1 * q2.a1 - q1.a2 * q2.a2 - q1.a3 * q2.a3 - q1.a4 * q2.a4,
+                q1.a1 * q2.a2 + q1.a2 * q2.a1 + q1.a3 * q2.a4 - q1.a4 * q2.a3,
+                q1.a1 * q2.a3 - q1.a2 * q2.a4 + q1.a3 * q2.a1 + q1.a4 * q2.a2,
+                q1.a1 * q2.a4 + q1.a2 * q2.a3 - q1.a3 * q2.a2 + q1.a4 * q2.a1
                 );
+        }
+        public static Quaternion32 operator *(Quaternion32 q1, float f)
+        {
+            return new Quaternion32(q1.a1 * f, q1.a2 * f, q1.a3 * f, q1.a4 * f);
+        }
+        public static Quaternion32 operator *(float f, Quaternion32 q1)
+        {
+            return q1 * f;
         }
 
         public static Quaternion32 operator /(Quaternion32 q1, float f)
         {
-            return new Quaternion32(q1.w / f, q1.x / f, q1.y / f, q1.z / f);
+            return new Quaternion32(q1.a1 / f, q1.a2 / f, q1.a3 / f, q1.a4 / f);
         }
-        #endregion
-
-        #region others
-
-        public float MagnitudeSquared
+        public static Quaternion32 operator /(float f, Quaternion32 q1)
         {
-            get { return w * w + x * x + y * y + z * z; }
+            //TODO : More robust divisionFdo
+            return f * q1.Inverse();
+        }
+        public static bool operator ==(Quaternion32 q1, Quaternion32 q2)
+        {
+            return q1.Equals(q2);
         }
 
-        public float Magnitude
+        public static bool operator !=(Quaternion32 q1, Quaternion32 q2)
+        {
+            return !(q1 == q2);
+        }
+        #endregion 
+        #region operations
+
+        public float NormSquared
+        {
+            get { return a1 * a1 + a2 * a2 + a3 * a3 + a4 * a4; }
+        }
+
+        public float Norm
         {
             //TODO : create more robust magnitude
-            get { return (float)Math.Sqrt(MagnitudeSquared); }
+            get { return (float)Math.Sqrt(NormSquared); }
         }
 
-        public float DotProduct(Quaternion32 q1, Quaternion32 q2)
+        public static float DotProduct(Quaternion32 q1, Quaternion32 q2)
         {
-            return q1.x * q2.x + q1.y * q2.y + q1.z * q2.z + q1.w * q2.w;
+            return q1.a2 * q2.a2 + q1.a3 * q2.a3 + q1.a4 * q2.a4 + q1.a1 * q2.a1;
         }
         public Quaternion32 Inverse()
         {
-            return Conjugate() / MagnitudeSquared;
+            return Conjugate() / NormSquared;
         }
         public Quaternion32 Normalize()
         {
-            var magnitude = Magnitude;
-            return new Quaternion32(w / magnitude, x / magnitude, y / magnitude, z / magnitude);
+            var norm = Norm;
+            return new Quaternion32(a1 / norm, a2 / norm, a3 / norm, a4 / norm);
         }
         public static Quaternion32 Normalize(Quaternion32 quat)
         {
@@ -121,25 +165,38 @@ namespace MathNet.Numerics
         }
         public Quaternion32 Conjugate()
         {
-            return new Quaternion32(w, -x, -y, -z);
+            return new Quaternion32(a1, -a2, -a3, -a4);
         }
         public static Quaternion32 Conjugate(Quaternion32 quat)
         {
             return quat.Conjugate();
         }
         #endregion
+
+        public bool IsNan
+        {
+            get { return float.IsNaN(a1) || float.IsNaN(a2) || float.IsNaN(a3) || float.IsNaN(a4); }
+        }
+
+        public bool IsInfinity
+        {
+            get { return float.IsInfinity(a1) || float.IsInfinity(a2) || float.IsInfinity(a3) || float.IsInfinity(a4); }
+        }
+
+        public static readonly Quaternion32 One = new Quaternion32(1, 0, 0, 0);
+        public static readonly Quaternion32 Zero = new Quaternion32(0, 0, 0, 0);
         public string ToString(string format, IFormatProvider formatProvider)
         {
             return string.Format(formatProvider, "({0}, {1}, {2}, {3})",
-                w.ToString(format, formatProvider),
-                x.ToString(format, formatProvider),
-                y.ToString(format, formatProvider),
-                z.ToString(format, formatProvider));
+                a1.ToString(format, formatProvider),
+                a2.ToString(format, formatProvider),
+                a3.ToString(format, formatProvider),
+                a4.ToString(format, formatProvider));
         }
 
         public bool Equals(Quaternion32 other)
         {
-            return w.AlmostEqual(other.w) && x.AlmostEqual(other.x) && y.AlmostEqual(other.y) && z.AlmostEqual(other.z);
+            return a1.AlmostEqual(other.a1) && a2.AlmostEqual(other.a2) && a3.AlmostEqual(other.a3) && a4.AlmostEqual(other.a4);
         }
     }
 }
---
+++ src/UnitTests/QuaternionsTests/Quaternions32Tests.cs
@@ -0,0 +1,49 @@
+﻿namespace MathNet.Numerics.UnitTests.QuaternionsTests
+{
+    using System.Security.Cryptography.X509Certificates;
+    using NUnit.Framework;
+    /// <summary>
+    /// Complex32 tests.
+    /// </summary>
+    /// <remarks>
+    /// Tests are based on http://web.cs.iastate.edu/~cs577/handouts/quaternion.pdf and my own calculations
+    /// </remarks>
+    [TestFixture]
+    public class Quaternions32Tests
+    {
+        /// <summary>
+        /// Can add a quaternions using operator.
+        /// </summary>
+        [Test]
+        public void CanAddQuaternionsUsingOperator()
+        {
+            Assert.AreEqual(new Quaternion32(1, 2, 3, 4) + new Quaternion32(1, 2, 3, 4), new Quaternion32(2, 4, 6, 8));
+            Assert.AreEqual(new Quaternion32(0, 0, 0, 0) + new Quaternion32(1, 2, 3, 4), new Quaternion32(1, 2, 3, 4));
+            Assert.AreEqual(new Quaternion32(0, 0, 0, 0) + Quaternion32.Zero, Quaternion32.Zero);
+            Assert.AreEqual(new Quaternion32(0, 0, 0, 0) + Quaternion32.One, Quaternion32.One);
+        }
+        /// <summary>
+        /// Can substract quaternions using operator.
+        /// </summary>
+        [Test]
+        public void CanSubstractQuaternionsUsingOperator()
+        {
+            Assert.AreEqual(Quaternion32.Zero,new Quaternion32(1, 2, 3, 4) - new Quaternion32(1, 2, 3, 4));
+            Assert.AreEqual(new Quaternion32(-1, -2, -3, -4),new Quaternion32(0, 0, 0, 0) - new Quaternion32(1, 2, 3, 4));
+            Assert.AreEqual(Quaternion32.Zero,new Quaternion32(0, 0, 0, 0) - Quaternion32.Zero);
+            //Assert.AreEqual(new Quaternion32(0, 0, 0, 0) - Quaternion32.One, -Quaternion32.One);
+        }
+
+        [Test]
+        public void CanMultiplyQuaternionsUsingOperator()
+        {
+            Assert.AreEqual(new Quaternion32(8, -9, -2, 11), new Quaternion32(3, 1, -2, 1) * new Quaternion32(2, -1, 2, 3));
+        }
+
+        [Test]
+        public void CanCalculateDotProduct()
+        {
+            Assert.AreEqual(new Quaternion32(0, -8, -4, 0),Quaternion32.DotProduct(new Quaternion32(3, 1, -2, 1), new Quaternion32(2, -1, 2, 3)));
+        }
+    }
+}
