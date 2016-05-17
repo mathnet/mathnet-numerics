40e32aaf576c59423f5fc2d4ad63331846688f10
Yoonku Hwang <yz0624@gmail.com>
2016-05-17 16:58:48 +0900

    add Adams-Bashforth First Order(same as Euler method) solver and test

---
+++ src/Numerics/OdeSolvers/AdamsBashforth.cs
@@ -0,0 +1,78 @@
+﻿// <copyright file="AdamsBashforth.cs" company="Math.NET">
+// Math.NET Numerics, part of the Math.NET Project
+// http://numerics.mathdotnet.com
+// http://github.com/mathnet/mathnet-numerics
+//
+// Copyright (c) 2009-2016 Math.NET
+//
+// Permission is hereby granted, free of charge, to any person
+// obtaining a copy of this software and associated documentation
+// files (the "Software"), to deal in the Software without
+// restriction, including without limitation the rights to use,
+// copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following
+// conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+// </copyright>
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MathNet.Numerics.OdeSolvers
+{
+    public static class AdamsBashforth
+    {
+
+        /// <summary>
+        /// First Order AB method(same as Forward Euler)
+        /// </summary>
+        /// <param name="y0">Initial value</param>
+        /// <param name="start">Start Time</param>
+        /// <param name="end">End Time</param>
+        /// <param name="N">Number of subintervals</param>
+        /// <param name="f">ode model</param>
+        /// <returns></returns>
+        public static double[] FirstOrder(double y0, double start, double end, int N, Func<double, double, double> f)
+        {
+            double dt = (end - start) / (N - 1);
+            double t = start;
+            double[] y = new double[N];
+            y[0] = y0;
+            for (int i = 1; i < N; i++)
+            {
+                y[i] = y0 + dt * f(t, y0);
+                t += dt;
+                y0 = y[i];
+            }
+            return y;
+        }
+
+        public static double[] SecondOrder()
+        {
+            return null;
+        }
+
+        public static double[] ThirdOrder()
+        {
+            return null;
+        }
+
+        public static double[] FourthOrder()
+        {
+            return null;
+        }
+    }
+}
