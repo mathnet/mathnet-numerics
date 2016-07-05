b115772d1128b004ef4dc38296ab9ddd42692728
Jon Smit <tmp.jbs.st@gmail.com>
2016-07-06 01:12:39 +0200

    Added another version of the Rsquared test.

--- src/Numerics/GoodnessOfFit.cs
+++ src/Numerics/GoodnessOfFit.cs
@@ -27,6 +27,8 @@
 
 using System.Collections.Generic;
 using MathNet.Numerics.Statistics;
+using System;
+using MathNet.Numerics.Properties;
 
 namespace MathNet.Numerics
 {
@@ -43,6 +45,51 @@ namespace MathNet.Numerics
         {
             var corr = Correlation.Pearson(modelledValues, observedValues);
             return corr * corr;
+        }
+
+        /// <summary>
+        /// Calculated the R-Squared value, also known as coefficient of determination,
+        /// given modelled and observed values
+        /// </summary>
+        /// <param name="modelledValues">The values expected from the modelled</param>
+        /// <param name="observedValues">The actual data set values obtained</param>
+        /// <returns>Squared Person product-momentum correlation coefficient.</returns>
+        public static double CoefficentOfDetermination(IEnumerable<double> modelledValues, IEnumerable<double> observedValues)
+        {
+            var y = observedValues;
+            var f = modelledValues;
+            int n = 0;
+
+            double meanY = 0;
+            double ssTot = 0;
+            double ssRes = 0;
+
+            // WARNING: do not try to "optimize" by summing up products instead of using differences.
+            // It would indeed be faster, but numerically much less robust if large mean + low variance.
+
+            using (IEnumerator<double> ieY = y.GetEnumerator())
+            using (IEnumerator<double> ieF = f.GetEnumerator()) {
+                while (ieY.MoveNext()) {
+                    if (!ieF.MoveNext()) {
+                        throw new ArgumentOutOfRangeException("dataB", Resources.ArgumentArraysSameLength);
+                    }
+
+                    double currentY = ieY.Current;
+                    double currentF = ieF.Current;
+
+                    double deltaY = currentY - meanY;
+                    double scaleDeltaY = deltaY/++n;
+
+                    meanY += scaleDeltaY;
+                    ssTot += scaleDeltaY*deltaY*(n - 1);
+                    ssRes += (currentY - currentF)*(currentY-currentF);
+                }
+
+                if (ieF.MoveNext()) {
+                    throw new ArgumentOutOfRangeException("dataA", Resources.ArgumentArraysSameLength);
+                }
+            }
+            return 1 - ssRes/ssTot;
         }
 
         /// <summary>
