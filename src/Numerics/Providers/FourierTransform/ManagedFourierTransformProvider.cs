7f5b414bbee7ce2fb1d635d255a88ca58689330d
Christoph Ruegg <git@cdrnet.ch>
2016-10-07 13:39:27 +0200

    FFT: fft provider mvp interface

---
+++ src/Numerics/Providers/FourierTransform/ManagedFourierTransformProvider.cs
@@ -0,0 +1,34 @@
+﻿using System.Numerics;
+using MathNet.Numerics.IntegralTransforms;
+
+namespace MathNet.Numerics.Providers.FourierTransform
+{
+    public class ManagedFourierTransformProvider : IFourierTransformProvider
+    {
+        public void ForwardInplace(Complex[] complex)
+        {
+            Fourier.BluesteinForward(complex, FourierOptions.Default);
+        }
+
+        public void BackwardInplace(Complex[] complex)
+        {
+            Fourier.BluesteinInverse(complex, FourierOptions.Default);
+        }
+
+        public Complex[] Forward(Complex[] complexTimeSpace)
+        {
+            Complex[] work = new Complex[complexTimeSpace.Length];
+            complexTimeSpace.Copy(work);
+            ForwardInplace(work);
+            return work;
+        }
+
+        public Complex[] Backward(Complex[] complexFrequenceSpace)
+        {
+            Complex[] work = new Complex[complexFrequenceSpace.Length];
+            complexFrequenceSpace.Copy(work);
+            BackwardInplace(work);
+            return work;
+        }
+    }
+}
