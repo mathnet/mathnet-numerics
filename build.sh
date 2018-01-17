ebbb63dd9481964dd2b0aaddcdd6bfcbe8b82429
Christoph Ruegg <git@cdrnet.ch>
2018-01-17 22:32:54 +0100

    Build: set FrameworkPathOverride when building on Linux

--- build.sh
+++ build.sh
@@ -3,13 +3,22 @@
 set -eu
 set -o pipefail
 
-cd `dirname $0`
+cd "$(dirname "$0")"
+
+PAKET_BOOTSTRAPPER_EXE=.paket/paket.bootstrapper.exe
+PAKET_EXE=.paket/paket.exe
+FAKE_EXE=packages/build/FAKE/tools/FAKE.exe
 
 FSIARGS=""
+FSIARGS2=""
 OS=${OS:-"unknown"}
 if [[ "$OS" != "Windows_NT" ]]
 then
-  FSIARGS="--fsiargs -d:MONO"
+  FSIARGS="--fsiargs"
+  FSIARGS2="-d:MONO"
+
+  # Allows NETFramework like net45 to be built using dotnet core tooling with mono
+  export FrameworkPathOverride=$(dirname $(which mono))/../lib/mono/4.5/
 fi
 
 function run() {
@@ -26,6 +35,6 @@ then
   mozroots --import --sync --quiet
 fi
 
-run .paket/paket.bootstrapper.exe
-run .paket/paket.exe restore
-run packages/build/FAKE/tools/FAKE.exe "$@" $FSIARGS build.fsx
+run $PAKET_BOOTSTRAPPER_EXE
+run $PAKET_EXE restore
+run $FAKE_EXE "$@" $FSIARGS $FSIARGS2 build.fsx
