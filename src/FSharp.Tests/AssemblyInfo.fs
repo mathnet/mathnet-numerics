82dcc548dadf13ee1ea43f08f2bd33a4126cb2e6
Christoph Ruegg <git@cdrnet.ch>
2018-02-11 10:57:11 +0100

    Release v4.0.0

--- src/FSharp.Tests/AssemblyInfo.fs
+++ src/FSharp.Tests/AssemblyInfo.fs
@@ -12,7 +12,7 @@ open System.Runtime.InteropServices
 
 [<assembly: AssemblyVersion("4.0.0.0")>]
 [<assembly: AssemblyFileVersion("4.0.0.0")>]
-[<assembly: AssemblyInformationalVersion("4.0.0-beta07")>]
+[<assembly: AssemblyInformationalVersion("4.0.0")>]
 
 [<assembly: ComVisible(false)>]
 [<assembly: Guid("C9AA6156-F799-42E4-B50D-2E88AD7D1750")>]
