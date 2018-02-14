3d23a1febcf485118c3b502a277f050fe33bd822
Christoph Ruegg <git@cdrnet.ch>
2018-02-14 19:56:14 +0100

    Release Data Extensions v4.0.0

--- src/Data.Tests/Properties/AssemblyInfo.cs
+++ src/Data.Tests/Properties/AssemblyInfo.cs
@@ -16,4 +16,4 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("4.0.0.0")]
 [assembly: AssemblyFileVersion("4.0.0.0")]
-[assembly: AssemblyInformationalVersion("4.0.0-beta01")]
+[assembly: AssemblyInformationalVersion("4.0.0")]
