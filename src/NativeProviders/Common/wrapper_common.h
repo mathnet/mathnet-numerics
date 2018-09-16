e432437c8f1bb1a62d4be4ee9c19bea7cbe49f69
Christoph Ruegg <git@cdrnet.ch>
2018-09-16 16:35:22 +0200

    MKL: upgrade to Intel Compiler 19

--- src/NativeProviders/Common/wrapper_common.h
+++ src/NativeProviders/Common/wrapper_common.h
@@ -2,7 +2,7 @@
 #define WRAPPER_COMMON_H
 
 #ifndef __MS_VC_INSTALL_PATH
-#define __MS_VC_INSTALL_PATH C:/Program Files (x86)/Microsoft Visual Studio/2017/Professional/VC/Tools/MSVC/14.14.26428
+#define __MS_VC_INSTALL_PATH C:/Program Files (x86)/Microsoft Visual Studio/2017/Professional/VC/Tools/MSVC/14.15.26726
 #endif
 
 #ifdef _WINDOWS
