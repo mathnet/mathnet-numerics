83681d2b8cba5bddef235e4bd3359e267cb11273
Daniel Sword <dsword@gmail.com>
2018-11-19 20:00:54 -0800

    Combinatorics: add a test class for new combinatoric methods.

---
+++ src/Numerics.Tests/CombinatoricsTests/PermutationTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace MathNet.Numerics.Tests.CombinatoricsTests
+{
+    [TestFixture]
+    public class PermutationTests
+    {
+        private IEqualityComparer<int[]> _comparer = new ArrayEqualityComparer();
+        class ArrayEqualityComparer : IEqualityComparer<int[]>
+        {
+            public bool Equals(int[] x, int[] y)
+            {
+                return StructuralComparisons.StructuralEqualityComparer.Equals(x, y);
+            }
+
+            public int GetHashCode(int[] obj)
+            {
+                return StructuralComparisons.StructuralEqualityComparer.GetHashCode(obj);
+            }
+        }
+
+    }
+}
