6dd8838766d619e2eca6e07ace3d1e717f6f2a46
Alex Hildebrand <hildebrand@till-id.com>
2020-01-19 23:29:11 +0100

    Testing Moore-Penrose Pseudo-Inverse

--- src/Numerics.Tests/LinearAlgebraTests/Complex32/MatrixTests.Arithmetic.cs
+++ src/Numerics.Tests/LinearAlgebraTests/Complex32/MatrixTests.Arithmetic.cs
@@ -821,6 +821,20 @@ namespace MathNet.Numerics.UnitTests.LinearAlgebraTests.Complex32
         }
 
         /// <summary>
+        /// Can compute Moore-Penrose Pseudo-Inverse.
+        /// </summary>
+        [TestCase("Square3x3")]
+        [TestCase("Wide2x3")]
+        [TestCase("Tall3x2")]
+        public virtual void CanComputePseudoInverse(string name)
+        {
+            var matrix = TestMatrices[name];
+            var inverse = matrix.PseudoInverse();
+            // Testing for Moore–Penrose conditions 1: A*A^+*A = A
+            AssertHelpers.AlmostEqual(matrix, matrix * inverse * matrix, 5);
+        }
+
+        /// <summary>
         /// Can calculate Kronecker product.
         /// </summary>
         [Test]
